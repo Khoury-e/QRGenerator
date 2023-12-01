d28b026e438994c0c17805a822ca57a86cf6c984
MoeSouki <mohamadelsouki49@gmail.com>
2023-12-01 11:13:53 +0200

    added some server side code to test if it works

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 import pyqrcode # must be installed using pip: pip install pyqrcode 
 import socket
+from PIL import Image
+import numpy as np
 
 IP = "localhost"
 port=12345 # random port number >1000
@@ -12,7 +14,24 @@ def generateQR(url):
 
     #save file as png/svg
     png = qr.png("name_of_image.png", scale=4) # save the image of the QR code with name of the image
+    
+    image_array = np.array(png) # turn the image into a numpy array
+    return image_array # returning the image
 
+serverSocket.listen (1)
+print (" Server listening on", IP , port )
+# Accept client ’s connection
+client_socket , client_address = port.accept()
+print (" Accepted connection from ", client_address)
+url = client_socket.recv(1024).decode()
+print (" Received from client ", url)
+
+image_matrix = generateQR(url)
+client_socket.send( image_matrix.encode())
+
+
+client_socket.close()
+serverSocket.close()
 '''
     these lines of codes represent the initialization of the socket of the server and the function
     generateQR(url) will generate the QR as a png image and will later on send the image to the client 
