35026049489e675d0ea4eceeed418c51d25fda33
MoeSouki <mohamadelsouki49@gmail.com>
2023-12-01 11:02:10 +0200

    adding the 2d matrix to image conversion
    I found a library called Pillow that would transform a 2d matrix into an image

--- client.py
+++ client.py
@@ -1,8 +1,20 @@
 import socket
+from PIL import Image # used to decode the image
+# use pip install pillow
+import numpy as np # pip install numpy
 server_ip = "localhost"
 server_port = 12345 # random socket number that is the server's sockrt number
 client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM) #This code creates a TCP client socket for IPv4 communication
 client_socket.connect((server_ip,server_port)) #connecting to the server socket
-url = input("Please Enter the URL: ")
-client_socket.send(url.encode())
+url = input("Please Enter the URL: ") #taking the url as input
+client_socket.send(url.encode()) # sending the url
+
+image_matrix = client_socket.recv(1024).decode() # reciving the 2d matrix from the server
+image_data = np.uint8(image_matrix)# Convert the matrix to uint8 data type 
+#(assuming pixel values are in the range 0-255)
+
+image = Image.fromarray(image_data)# Create an image from the matrix
+
+image.save("output_image.png") # save the image
+image.show() #load the image
 
